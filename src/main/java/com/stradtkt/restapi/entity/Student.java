6d657384f1e47e9d3f7b326ea837912bb180c863
kevinstradtman <stradtkt@outlook.com>
2023-07-25 19:30:12 -0400

    Initial Commit

---
+++ src/main/java/com/stradtkt/restapi/entity/Student.java
@@ -0,0 +1,13 @@
+package com.stradtkt.restapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/stradtkt/restapi/rest/StudentRestController.java
@@ -0,0 +1,42 @@
+package com.stradtkt.restapi.rest;
+
+import com.stradtkt.restapi.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("Kevin", "Smith"));
+        students.add(new Student("Kyle", "Setters"));
+        students.add(new Student("Patrick", "Jones"));
+        students.add(new Student("Lee", "Petterson"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        if((studentId >= students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id was not in the system please try again!");
+        }
+        return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        return null;
+    }
+}
